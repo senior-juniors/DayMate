6d4174dd6a4477dabd8514a2c391ad6c4f49b7b4
mundo <mundotiyadeepesh@gmail.com>
2026-01-18 17:03:38 +0530

    Add Study Material feature with Google Drive folder mapping

---
+++ app/src/main/java/com/example/daymate/Screens/DriveData.kt
@@ -0,0 +1,47 @@
+package com.example.daymate.Screens
+
+object DriveData {
+    // Mapping: "Semester Name" -> Map("Branch Name" -> "URL")
+    val links = mapOf(
+        "1 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/1cGBdT9gUmA2pPkfoq9u4b9me1jwwiMgG",
+            "ECE" to "https://drive.google.com/drive/folders/1BwRuhv-YxcNtys78zklky5BqIXAuvZID",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S1_AIDE"
+        ),
+        "2 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/LINK_S2_CSE",
+            "ECE" to "https://drive.google.com/drive/folders/LINK_S2_ECE",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S2_AIDE"
+        ),
+        "3 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/LINK_S3_CSE",
+            "ECE" to "https://drive.google.com/drive/folders/LINK_S3_ECE",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S3_AIDE"
+        ),
+        "4 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/LINK_S4_CSE",
+            "ECE" to "https://drive.google.com/drive/folders/LINK_S4_ECE",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S4_AIDE"
+        ),
+        "5 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/LINK_S5_CSE",
+            "ECE" to "https://drive.google.com/drive/folders/LINK_S5_ECE",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S5_AIDE"
+        ),
+        "6 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/LINK_S6_CSE",
+            "ECE" to "https://drive.google.com/drive/folders/LINK_S6_ECE",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S6_AIDE"
+        ),
+        "7 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/LINK_S7_CSE",
+            "ECE" to "https://drive.google.com/drive/folders/LINK_S7_ECE",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S7_AIDE"
+        ),
+        "8 Semester" to mapOf(
+            "CSE" to "https://drive.google.com/drive/folders/LINK_S8_CSE",
+            "ECE" to "https://drive.google.com/drive/folders/LINK_S8_ECE",
+            "AIDE" to "https://drive.google.com/drive/folders/LINK_S8_AIDE"
+        )
+    )
+}
